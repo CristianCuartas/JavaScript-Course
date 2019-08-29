75fa5ee8f5f3bf722881c8f4f958664b28f947b0
CristianCuartas <cristianhz1109@gmail.com>
2019-08-29 07:51:22 -0500

    Cambios

--- Sección 31 Node JS Express Sequelize Pug/server/config/database.js
+++ Sección 31 Node JS Express Sequelize Pug/server/config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-module.exports = new Sequelize('agenciadeviajes','root', 'root', {
+module.exports = new Sequelize('agenciadeviajes','root', '', {
     host: '127.0.0.1',
-    port: '3307',
+    port: '3306',
     dialect: 'mysql',
     define:{
         timestamps: false
